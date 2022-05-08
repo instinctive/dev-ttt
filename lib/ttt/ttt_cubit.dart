52e10ddc56a0d80d49816074d0b56acc89f49415
instinctive <177694+instinctive@users.noreply.github.com>
2022-05-08 16:22:23 -0700

    Add cubit for ttt

---
+++ lib/ttt/ttt_cubit.dart
@@ -0,0 +1,14 @@
+import 'package:bloc/bloc.dart';
+
+import 'package:ttt/ttt/ttt_model.dart';
+
+class TicTacToeCubit extends Cubit<TicTacToe> {
+  TicTacToeCubit(): super (TicTacToe.init);
+  void init() => emit(TicTacToe.init);
+  void move(int i) {
+    try {
+      emit(state.move(i));
+    } on InvalidMove catch (_) {
+    }
+  }
+}
